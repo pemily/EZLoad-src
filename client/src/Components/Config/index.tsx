b635fb6cc8dfed3e72beb6d71fa3a2a06b2ca688
pemily <pascal.emily@gmail.com>
2022-05-26 15:06:55 +0200

    add copyright

--- client/src/Components/Config/index.tsx
+++ client/src/Components/Config/index.tsx
@@ -1,3 +1,20 @@
+/*
+ * ezClient - EZLoad an automatic loader for EZPortfolio
+ * Copyright © 2021 EMILY Pascal (pascal.emily@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 import { Anchor, Box, Heading, Form, Button, Text, CheckBox, Table, TableHeader, TableRow, TableCell, TableBody, Markdown, Layer, FileInput } from "grommet";
 import { Add, Trash, Validate, SchedulePlay, Upload } from 'grommet-icons';
 import { saveEzProfile, savePassword, jsonCall, ezApi, getChromeVersion, valued, saveMainSettings } from '../../ez-api/tools';
